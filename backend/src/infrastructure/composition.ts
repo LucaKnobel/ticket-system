77f9d70afcd4c251f237c4ee4f5d6f64f2375deb
Luca Knobel <knobel.luca@gmail.com>
2026-08-02 13:04:51 +0000

    feat: implement login user use case with session creation and validation

---
+++ backend/src/infrastructure/composition.ts
@@ -0,0 +1,11 @@
+import { argon2PasswordHasher } from "@infrastructure/password-hasher/argon2-password-hasher.js";
+import { buildLoginUser } from "@application/services/build-login-user.js";
+
+import { prismaUserRepository } from "@infrastructure/db/repositories/prisma-user-repository.js";
+import { prismaSessionRepository } from "@infrastructure/db/repositories/prisma-session-repository.js";
+
+export const loginUser = buildLoginUser(
+  prismaUserRepository,
+  prismaSessionRepository,
+  argon2PasswordHasher,
+);
